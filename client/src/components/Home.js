247485854de4b2e7528dbd347ba9f145c9831163
Andrei <andsemenov@gmail.com>
2020-11-01 20:38:59 +0000

    Create applicant button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 import "../styles/Home.css";
+import "semantic-ui-css/semantic.min.css";
 
 const Home = () => {
+  const history = useHistory();
+
+  function loginClick() {
+    history.push("/applicant_login");
+  }
+
   return (
     <div>
       <header className="home-header">
         <h1>Diversi-Hire</h1>
       </header>
+      <Button primary onClick={loginClick}>
+        I am an applicant
+      </Button>
     </div>
   );
 };
