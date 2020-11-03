74b6147e7b2e73459f73e1a77059450b1b3cc310
Andrei <andsemenov@gmail.com>
2020-11-03 20:13:34 +0000

    Create applicant button - used Link & fixed text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,17 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 import "../styles/Home.css";
 import "semantic-ui-css/semantic.min.css";
 
 const Home = () => {
-  const history = useHistory();
-
-  function loginClick() {
-    history.push("/applicant_login");
-  }
-
   return (
     <div>
       <header className="home-header">
         <h1>Diversi-Hire</h1>
       </header>
-      <Button primary onClick={loginClick}>
-        I am an applicant
+      <Button primary as={Link} to="/applicant_login">
+        Log in as Applicant
       </Button>
     </div>
   );
