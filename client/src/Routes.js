a8bbc7203974878f643cd593e4822d889a24606f
S R <sharlu@Ss-MacBook.broadband>
2020-11-05 19:55:55 +0000

    Set active props on the active links only

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,31 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { List } from "semantic-ui-react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Status from "./components/Status";
+import MenuHeader from "./components/MenuHeader";
 
 const Routes = () => {
   return (
     <Router>
-      <div>
-        <List link>
-          <List.Item to="/" active as={Link}>
-            Home
-          </List.Item>
-          <List.Item to="/about" as={Link}>
-            About
-          </List.Item>
-          <List.Item to="/status" as={Link}>
-            Status
-          </List.Item>
-        </List>
-        <div>
-          <Route path="/" exact component={Home} />
-          <Route path="/about/" component={About} />
-          <Route path="/status/" component={Status} />
-        </div>
-      </div>
+      <MenuHeader />
+      <Route path="/" exact component={Home} />
+      <Route path="/about/" component={About} />
+      <Route path="/status/" component={Status} />
     </Router>
   );
 };
