f7fcfd2e1111e585bb13450d224222e58d91806b
Andrei <andsemenov@gmail.com>
2020-11-07 21:01:24 +0000

    Create profile creation - server

--- server/api/profile.js
+++ server/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 /** route is /profile/ (status is defined as prefix in index.js) */
 router.post("/", (req, res) => {
+  console.log(req);
+  console.log(res);
   res.send("All good");
 });
 
