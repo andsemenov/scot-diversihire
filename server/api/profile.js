bdc44f94557f5cc0f7e78984a4f4ca508ba1e455
S R <sharlu@Ss-MacBook.broadband>
2020-11-12 20:52:41 +0000

    Update merge conflict

--- server/api/profile.js
+++ server/api/profile.js
@@ -8,38 +8,22 @@ const { nanoid } = require("nanoid");
 const passport = require("passport");
 
 /** route is /profile/ (status is defined as prefix in index.js) */
-router.post("/", async(req, res) => {
+router.post("/", passport.authenticate("jwt", { session: false }), async(req, res) => {
   try {
     const db_newProfile = req.body;
+    db_newProfile.applicant_id = req.user.id;
+    db_newProfile.profile_public_id = nanoid();
     const { experiences } = req.body;
     const response = await createProfile(db_newProfile);
     await experiences.forEach((experience) => {
-      createWorkExperience({ ...experience, profile_id: response[0].id});
+      createWorkExperience({ ...experience, profile_id: response[0].id });
     });
     
     res.status(201).send("Profile created");
   } catch (error) {
+    console.log("error", error);
     res.status(500).send("Error profile creation is unsuccessful");
   }
 });
-
-router.post(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const db_newProfile = req.body;
-    db_newProfile.applicant_id = req.user.id;
-    db_newProfile.profile_public_id = nanoid();
-
-    createProfile(db_newProfile)
-      .then((data) => {
-        res.sendStatus(201);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
-  }
-);
 
 module.exports = router;
