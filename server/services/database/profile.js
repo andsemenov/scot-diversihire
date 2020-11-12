d402f08d3b361c4155a1381eafc413eee16357de
Marina <mara.berezin@gmail.com>
2020-11-12 23:32:19 +0000

    delete database/profiles.js

--- server/services/database/profile.js
+++ server/services/database/profile.js
@@ -5,8 +5,10 @@ const pool = new Pool(config);
 const createProfile = (profile) => {
 	return pool
 		.query(
-			"INSERT INTO profile (job_title, bio, location, employment_status) values ($1, $2, $3, $4)",
+			"INSERT INTO profile (profile_public_id, applicant_id, job_title, bio, location, employment_status) values ($1, $2, $3, $4, $5, $6) returning *",
 			[
+				profile.profile_public_id,
+				profile.applicant_id,
 				profile.job_title,
 				profile.bio,
 				profile.location,
