bbee1c38a3b9758572027c424f0607289cc6a191
S R <sharlu@Ss-MacBook.broadband>
2020-11-12 03:31:01 +0000

    Modify server API to accept an array of experiences but not able to save in db table

--- server/api/profile.js
+++ server/api/profile.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const { createProfile } = require("../services/database/profile");
+const {
+  createWorkExperience
+} = require("../services/database/work_experience");
 const router = express.Router();
 
 /** route is /profile/ (status is defined as prefix in index.js) */
-router.post("/", (req, res) => {
-  const db_newProfile = req.body;
-  createProfile(db_newProfile);
-  res.status(201).send("Profile created");
+router.post("/", async(req, res) => {
+  try {
+    const db_newProfile = req.body;
+    const { experiences } = req.body;
+    await experiences.forEach((experience) => {
+      createWorkExperience(experience);
+    });
+    await createProfile(db_newProfile);
+    res.status(201).send("Profile created");
+  } catch (error) {
+    res.status(500).send("Error profile creation is unsuccessful");
+  }
 });
 
 module.exports = router;
