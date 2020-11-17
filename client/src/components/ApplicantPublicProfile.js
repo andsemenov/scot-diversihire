a4193a9b458a9fd77c0581f13db8a18d6650b7cc
Andrei <andsemenov@gmail.com>
2020-11-17 22:59:33 +0000

    View applicant public profile - need to finish a form

--- client/src/components/ApplicantPublicProfile.js
+++ client/src/components/ApplicantPublicProfile.js
@@ -1,36 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "semantic-ui-react";
-import { Redirect } from "react-router-dom";
 import { getProfile } from "../api/profiles";
-import Experience from "./Experience";
+import moment from "moment";
 
-const ApplicantPublicProfile = () => {
-  return (
-    <Form>
-      <Form.TextArea
-        name="bio"
-        label="Bio"
-        placeholder="Tell us more about you..."
-      />
+const ApplicantPublicProfile = ({ match }) => {
+  const publicId = match.params.public_id;
 
-      <Form.Field>
-        <label htmlFor="job_title">Job title</label>
-        <input id="job_title" name="job_title" placeholder="Job title" />
-      </Form.Field>
-      <p>array of exp</p>
-      <Form.Field>
-        <label htmlFor="years_experience">Years Experience</label>
-        <input
-          id="years_experience"
-          name="years_experience"
-          placeholder="years_experience"
-        />
-      </Form.Field>
-      <p>array of edu</p>
+  const [profile, setProfile] = useState(null);
+  useEffect(() => {
+    getProfile(publicId).then(data => setProfile(data));
+  }, [publicId]);
 
-      <Form.Button primary>Contact Candidate</Form.Button>
-    </Form>
-  );
+  if (profile) {
+    return (
+      <Form>
+        <Form.TextArea name="bio" label="Bio" defaultValue={profile.bio} />
+        <Form.Field>
+          <label htmlFor="job_title">Job title</label>
+          <input id="job_title" defaultValue={profile.job_title} />
+        </Form.Field>
+
+        <div>
+          <p>
+            <strong>Experience</strong>
+          </p>
+          {profile.experience.map((experience, key) => (
+            <div key={key + "divtop"}>
+              <div key={key + "div"} className="ui raised segment">
+                <p key={key + "title"}>{experience.job_title}</p>
+                <p key={key + "company"}>{experience.company}</p>
+                <p key={key + "description"}>{experience.description}</p>
+              </div>
+              <Form.Field>
+                <label key={key + "label"} htmlFor="years_experience">
+                  Years Experience
+                </label>
+                <input
+                  key={key + "input"}
+                  id="years_experience"
+                  defaultValue={numberYears(
+                    experience.start_date,
+                    experience.end_date
+                  )}
+                />
+              </Form.Field>
+            </div>
+          ))}
+        </div>
+        <Form.Button primary>Contact Candidate</Form.Button>
+      </Form>
+    );
+  } else return <p>Loading</p>;
 };
 
 export default ApplicantPublicProfile;
+
+const numberYears = (start_date, end_date) => {
+  const start = moment(start_date);
+  const end = moment(end_date);
+  return end.diff(start, "years");
+};
