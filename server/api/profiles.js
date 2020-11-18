fd8692e347e164f57800169e45c6c47a656c901f
Marina <mara.berezin@gmail.com>
2020-11-18 14:15:03 +0000

    spaces 2

--- server/api/profiles.js
+++ server/api/profiles.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const {
-
-	createProfile,
-	getAllProfiles,
-	getProfileByPublicId,
+  createProfile,
+  getAllProfiles,
+  getProfileByPublicId,
 } = require("../services/database/profile");
 const {
-	createWorkExperience,
-	getWorkExperiencesByProfileId,
-	getEducationByProfileId,
+  createWorkExperience,
+  getWorkExperiencesByProfileId,
+  getEducationByProfileId,
 } = require("../services/database/work_experience");
 const router = express.Router();
 const { nanoid } = require("nanoid");
 const passport = require("passport");
-
 
 /** route is /profiles/ (status is defined as prefix in index.js) */
 router.post(
@@ -27,7 +25,7 @@ router.post(
       const { experiences } = req.body;
       const newProfile = await createProfile(db_newProfile);
       await Promise.all(
-        experiences.map((experience) =>
+        experiences.map(experience =>
           createWorkExperience({ ...experience, profile_id: newProfile.id })
         )
       );
@@ -41,50 +39,50 @@ router.post(
 
 router.get("/", (req, res) => {
   getAllProfiles()
-    .then((data) => {
+    .then(data => {
       res.send(data);
     })
-    .catch((err) => {
+    .catch(err => {
       console.error(err);
       res.send(500);
     });
 });
 
 router.get("/:public_id", (req, res) => {
-	const id = req.params.public_id;
-	getProfileByPublicId(id)
-		.then(profile => {
-			if (profile == null) {
-				return res.sendStatus(404);
-			}
+  const id = req.params.public_id;
+  getProfileByPublicId(id)
+    .then(profile => {
+      if (profile == null) {
+        return res.sendStatus(404);
+      }
 
-			const profileId = profile.id;
+      const profileId = profile.id;
 
-			getEducationByProfileId(profileId)
-				.then(educations => {
-					getWorkExperiencesByProfileId(profileId)
-						.then(experiences => {
-							profile = {
-								...profile,
-								education: educations,
-								work_experience: experiences,
-							};
-							res.send(profile);
-						})
-						.catch(err => {
-							console.error(err);
-							res.sendStatus(500);
-						});
-				})
-				.catch(err => {
-					console.error(err);
-					res.sendStatus(500);
-				});
-		})
-		.catch(err => {
-			console.error(err);
-			res.sendStatus(500);
-		});
+      getEducationByProfileId(profileId)
+        .then(educations => {
+          getWorkExperiencesByProfileId(profileId)
+            .then(experiences => {
+              profile = {
+                ...profile,
+                education: educations,
+                work_experience: experiences,
+              };
+              res.send(profile);
+            })
+            .catch(err => {
+              console.error(err);
+              res.sendStatus(500);
+            });
+        })
+        .catch(err => {
+          console.error(err);
+          res.sendStatus(500);
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
