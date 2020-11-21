7f1e1e4848e00420b6a1205fd7c9883c5f9d1aef
Andrei <andsemenov@gmail.com>
2020-11-21 00:05:04 +0000

    View applicant public profile - design of the page

--- client/src/components/ApplicantPublicProfile.js
+++ client/src/components/ApplicantPublicProfile.js
@@ -13,92 +13,107 @@ const ApplicantPublicProfile = ({ match }) => {
 
   if (profile) {
     return (
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width="10">
-            <Label as="label" color="white" ribbon="left">
-              Job title
-            </Label>
-            <Segment>{profile.job_title}</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Form.Button primary>Contact Candidate</Form.Button>
-          </Grid.Column>
-        </Grid.Row>
+      <Segment>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={12}>
+              <Label>Job Title</Label>
+              <Segment>{profile.job_title}</Segment>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Form.Button primary>Contact Candidate</Form.Button>
+            </Grid.Column>
+          </Grid.Row>
 
-        <Grid.Row>
-          <Grid.Column>
-            <Label>Bio</Label>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width="10">
-            <Segment>{profile.bio}</Segment>
-          </Grid.Column>
-        </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Label>Bio</Label>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width="12">
+              <Segment>{profile.bio}</Segment>
+            </Grid.Column>
+          </Grid.Row>
 
-        <Grid.Row>
-          <Grid.Column>
-            <Label>Work Experience</Label>
-          </Grid.Column>
-        </Grid.Row>
-
-        {profile.experiences.map((experience, key) => (
-          <Grid.Column
-            width="10"
-            key={key}
-            style={{
-              border: "dotted",
-              borderRadius: "30px"
-            }}
-          >
-            <Label key={key + "lab2"}>Position</Label>
-            <Segment key={key + "seg3"}>{experience.job_title}</Segment>
-            <Label key={key + "lab4"}>Company</Label>
-            <Segment key={key + "seg5"}>{experience.company}</Segment>
-            <Label key={key + "lab6"}>Description</Label>
-            <Segment key={key + "seg7"}>{experience.description}</Segment>
-
-            <Label key={key + "lab8"}>Employment Date</Label>
-
-            <Grid.Row columns={2} divided="vertically">
-              <Grid.Column>
-                <Segment key={key + "seg9"}>
-                  {moment(experience.start_date).format("MMMM YYYY")}
-                </Segment>
-              </Grid.Column>
-              <Grid.Column>
-                <Segment key={key + "seg10"}>
-                  {moment(experience.end_date).format("MMMM YYYY")}
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid.Column>
-        ))}
-
-        {/*  <Label>Education</Label>
-
-        {profile.educations.map((education, key) => (
-          <Grid.Column
-            style={{
-              border: "dotted",
-              borderRadius: "10px",
-            }}
-          >
-            <Label>Institution</Label>
-            <Segment>{education.institution}</Segment>
-            <Label>Qualification</Label>
-            <Segment>{education.qualification}</Segment>
-            <Label>Course title</Label>
-            <Segment>{education.course_title}</Segment>
-            <Label>Education Date</Label>
-            <Segment>
-              {moment(education.start_date).format("MMMM YYYY")}
-            </Segment>
-            <Segment>{moment(education.end_date).format("MMMM YYYY")}</Segment>
-          </Grid.Column>
-        ))} */}
-      </Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Label>Work Experience</Label>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Segment>
+          {profile.experiences.map((experience, key) => (
+            <Grid
+              key={key}
+              style={{
+                border: "dotted",
+                borderRadius: "30px"
+              }}
+            >
+              <Grid.Row>
+                <Grid.Column width="12">
+                  <Label key={key + "lab2"}>Position</Label>
+                  <Segment key={key + "seg3"}>{experience.job_title}</Segment>
+                  <Label key={key + "lab4"}>Company</Label>
+                  <Segment key={key + "seg5"}>{experience.company}</Segment>
+                  <Label key={key + "lab6"}>Description</Label>
+                  <Segment key={key + "seg7"}>{experience.description}</Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <Segment key={key + "seg9"}>
+                    {moment(experience.start_date).format("MMMM YYYY")}
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column>
+                  <Segment key={key + "seg10"}>
+                    {moment(experience.end_date).format("MMMM YYYY")}
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          ))}
+        </Segment>
+        <Label>Education</Label>
+        <Segment>
+          {profile.educations.map((education, key) => (
+            <Grid
+              key={key}
+              style={{
+                border: "dotted",
+                borderRadius: "30px"
+              }}
+            >
+              <Grid.Row>
+                <Grid.Column>
+                  <Label key={key + "lab2"}>Institution</Label>
+                  <Segment key={key + "seg3"}>{education.institution}</Segment>
+                  <Label key={key + "lab4"}>Qualification</Label>
+                  <Segment key={key + "seg5"}>
+                    {education.qualification}
+                  </Segment>
+                  <Label key={key + "lab6"}>Course title</Label>
+                  <Segment key={key + "seg7"}>{education.course_title}</Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <Segment key={key + "seg9"}>
+                    {moment(education.start_date).format("MMMM YYYY")}
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column>
+                  <Segment key={key + "seg10"}>
+                    {moment(education.end_date).format("MMMM YYYY")}
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          ))}
+        </Segment>
+      </Segment>
     );
   } else return <p>Loading</p>;
 };
