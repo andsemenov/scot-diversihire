9f30203e5684c513e1e87073599fcdde1cf1552e
Andrei <andsemenov@gmail.com>
2020-11-21 20:37:24 +0000

    View applicant public profile - changed grid

--- client/src/components/ApplicantPublicProfile.js
+++ client/src/components/ApplicantPublicProfile.js
@@ -12,6 +12,9 @@ const ApplicantPublicProfile = ({ match }) => {
   }, [publicId]);
 
   if (profile) {
+    if (profile === {}) {
+      return <p>No profile exists</p>;
+    }
     return (
       <Segment>
         <Grid>
@@ -42,75 +45,73 @@ const ApplicantPublicProfile = ({ match }) => {
           </Grid.Row>
 
           <Grid.Row>
-            <Grid.Column>
-              <Label>Work Experience</Label>
-            </Grid.Column>
+            <Label>Work Experience</Label>
           </Grid.Row>
         </Grid>
 
         {profile.experiences.map((experience, key) => (
           <Grid key={key}>
-            <Grid.Row key={key + "row1"}>
-              <Grid.Column key={key + "col2"} width="12">
-                <Label key={key + "lab3"}>Position</Label>
-                <Segment key={key + "seg4"}>{experience.job_title}</Segment>
-                <Label key={key + "lab5"}>Company</Label>
-                <Segment key={key + "seg6"}>{experience.company}</Segment>
-                <Label key={key + "lab7"}>Description</Label>
-                <Segment key={key + "seg8"}>{experience.description}</Segment>
-              </Grid.Column>
-            </Grid.Row>
-
-            <Label key={key + "lab9"}>Employment Date</Label>
-            <Grid.Row key={key + "row10"} columns={2}>
-              <Grid.Column key={key + "col11"} width="3">
-                <Segment key={key + "seg9"}>
-                  {moment(experience.start_date).format("MMMM YYYY")}
-                </Segment>
-              </Grid.Column>
-              <Grid.Column key={key + "col12"} width={3}>
-                <Segment key={key + "seg13"}>
-                  {moment(experience.end_date).format("MMMM YYYY")}
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
+            <Grid.Column key={key + "col2"} width={12}>
+              <Label key={key + "lab3"}>Position</Label>
+              <Segment key={key + "seg4"}>{experience.job_title}</Segment>
+              <Label key={key + "lab5"}>Company</Label>
+              <Segment key={key + "seg6"}>{experience.company}</Segment>
+              <Label key={key + "lab7"}>Description</Label>
+              <Segment key={key + "seg8"}>{experience.description}</Segment>
+              <Label key={key + "lab9"}>Employment Date</Label>
+              <Segment key={key + "seg10"}>
+                <Grid key={key + "gr11"}>
+                  <Grid.Row key={key + "lab12"} columns={2}>
+                    <Grid.Column key={key + "col13"} width={3}>
+                      <Segment key={key + "seg14"}>
+                        {moment(experience.start_date).format("MMMM YYYY")}
+                      </Segment>
+                    </Grid.Column>
+                    <Grid.Column key={key + "col15"} width={3}>
+                      <Segment key={key + "seg16"}>
+                        {moment(experience.end_date).format("MMMM YYYY")}
+                      </Segment>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Segment>
+            </Grid.Column>
           </Grid>
         ))}
+
         <Grid>
           <Grid.Row>
-            <Grid.Column>
-              <Label>Education</Label>
-            </Grid.Column>
+            <Label>Education</Label>
           </Grid.Row>
         </Grid>
 
         {profile.educations.map((education, key) => (
           <Grid key={key}>
-            <Grid.Row key={key + "row1"}>
-              <Grid.Column key={key + "col2"} width={12}>
-                <Label key={key + "lab3"}>Institution</Label>
-                <Segment key={key + "seg4"}>{education.institution}</Segment>
-                <Label key={key + "lab5"}>Qualification</Label>
-                <Segment key={key + "seg6"}>{education.qualification}</Segment>
-                <Label key={key + "lab7"}>Course title</Label>
-                <Segment key={key + "seg8"}>{education.course_title}</Segment>
-              </Grid.Column>
-            </Grid.Row>
-
-            <Label key={key + "lab9"}>Education Date</Label>
-
-            <Grid.Row key={key + "lab10"} columns={2}>
-              <Grid.Column key={key + "col11"} width={3}>
-                <Segment key={key + "seg12"}>
-                  {moment(education.start_date).format("MMMM YYYY")}
-                </Segment>
-              </Grid.Column>
-              <Grid.Column key={key + "col13"} width={3}>
-                <Segment key={key + "seg14"}>
-                  {moment(education.end_date).format("MMMM YYYY")}
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
+            <Grid.Column key={key + "col2"} width={12}>
+              <Label key={key + "lab3"}>Institution</Label>
+              <Segment key={key + "seg4"}>{education.institution}</Segment>
+              <Label key={key + "lab5"}>Qualification</Label>
+              <Segment key={key + "seg6"}>{education.qualification}</Segment>
+              <Label key={key + "lab7"}>Course title</Label>
+              <Segment key={key + "seg8"}>{education.course_title}</Segment>
+              <Label key={key + "lab9"}>Education Date</Label>
+              <Segment key={key + "seg10"}>
+                <Grid key={key + "gr11"}>
+                  <Grid.Row key={key + "lab12"} columns={2}>
+                    <Grid.Column key={key + "col13"} width={3}>
+                      <Segment key={key + "seg14"}>
+                        {moment(education.start_date).format("MMMM YYYY")}
+                      </Segment>
+                    </Grid.Column>
+                    <Grid.Column key={key + "col15"} width={3}>
+                      <Segment key={key + "seg16"}>
+                        {moment(education.end_date).format("MMMM YYYY")}
+                      </Segment>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Segment>
+            </Grid.Column>
           </Grid>
         ))}
       </Segment>
